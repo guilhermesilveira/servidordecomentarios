9430c7282ea6dc820dc8002074ff0b7df7b62011
guilherme silveira <guilherme.silveira@caelum.com.br>
2017-02-24 11:03:01 -0300

    apagando antigos do mes passado

--- app/controllers/locals_controller.rb
+++ app/controllers/locals_controller.rb
@@ -9,7 +9,7 @@ class LocalsController < ApplicationController
   # POST /locals
   # POST /locals.json
   def create
-    Local.where("created_at < :ultimo_mes", {ultimo_mes: 1.second.ago}).each do |local|
+    Local.where("created_at < :ultimo_mes", {ultimo_mes: 1.month.ago}).each do |local|
       local.comentarios.destroy_all
       local.destroy
     end
