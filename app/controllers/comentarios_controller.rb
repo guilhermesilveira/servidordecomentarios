1615bffe777147c3779e701e606af5e86df9d5e8
guilherme silveira <guilherme.silveira@caelum.com.br>
2017-02-27 09:25:18 -0300

    404 implementado

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -1,18 +1,9 @@
 class ComentariosController < ApplicationController
-  before_action :set_comentario, only: [:show, :edit, :update, :destroy]
 
-  # POST /comentarios
-  # POST /comentarios.json
   def create
     p = comentario_params
     l = Local.where("id = :id", { id: p[:local_id]}).first
     @comentario = Comentario.new({autor: p[:autor], texto: p[:texto], local: l})
-
-    puts "local"
-    puts p.inspect
-    puts @comentario.local
-    puts @comentario.inspect
-    puts "fim"
 
     if @comentario.save
       redirect_to @comentario.local
@@ -22,12 +13,7 @@ class ComentariosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comentario
-      @comentario = Comentario.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def comentario_params
       params.permit(:autor, :texto, :local_id)
     end
--- app/controllers/locals_controller.rb
+++ app/controllers/locals_controller.rb
@@ -1,13 +1,11 @@
 class LocalsController < ApplicationController
-  before_action :set_local, only: [:show, :edit, :update, :destroy]
 
   # GET /locals/1
-  # GET /locals/1.json
   def show
+      @local = Local.find(params[:id])
   end
 
   # POST /locals
-  # POST /locals.json
   def create
     Local.where("created_at < :ultimo_mes", {ultimo_mes: 1.month.ago}).each do |local|
       local.comentarios.destroy_all
@@ -22,12 +20,7 @@ class LocalsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_local
-      @local = Local.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def local_params
       params.fetch(:local, {})
     end
